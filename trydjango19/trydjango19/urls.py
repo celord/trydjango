56dfd673b05cd5f9346a9e1fa7cffd7014a612e4
celord <celord@gataca>
2017-05-02 14:02:31 -0600

    reanme again

--- trydjango19/trydjango19/urls.py
+++ trydjango19/trydjango19/urls.py
@@ -1,4 +1,4 @@
-"""src URL Configuration
+"""trydjango19 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.9/topics/http/urls/
