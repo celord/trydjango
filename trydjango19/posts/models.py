a51fd4181b33029e9bdc01dc7100f7e7c7b32e17
celord@gmail.com <celord@gmail.com>
2017-05-10 15:50:35 -0600

    adding urls update

--- trydjango19/posts/models.py
+++ trydjango19/posts/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from django.test.signals import update_connections_time_zone
+from django.core.urlresolvers import reverse
 
 
 # Create your models here.
@@ -11,8 +11,10 @@ class Post(models.Model):
     content = models.TextField()
     updated = models.DateField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
-    
-    
+
     def __unicode__(self):
         return self.title
-    
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"id": self.id})
+#         return "/posts/%s/" %(self.id)
