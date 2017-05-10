bfacb040e76123d00faedd61210fabe15bca0766
celord@gmail.com <celord@gmail.com>
2017-05-10 08:14:49 -0600

    From console

---
+++ trydjango19/posts/urls.py
@@ -0,0 +1,18 @@
+from django.conf.urls import url
+from django.contrib import admin
+
+from .views import (
+    posts_list,
+    posts_create,
+    posts_detail,
+    posts_delete,
+    posts_update)
+
+urlpatterns = [
+    url(r'^$',posts_list),
+    url(r'^delete/$',posts_delete),
+    url(r'^create/$',posts_create),
+    url(r'^(?P<id>\d+)/$',posts_detail, name='detail'), 
+    url(r'^update/$',posts_update),
+     
+]
