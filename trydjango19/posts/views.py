e5a0031c820515e9c31ea4dde2402b868dc60c73
Cesar Garcia <celord@gmail.com>
2017-05-18 14:39:04 -0600

    Adding pagination

--- trydjango19/posts/views.py
+++ trydjango19/posts/views.py
@@ -1,6 +1,7 @@
 from django.http import  HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .form import PostForm
 from .models import Post
 
@@ -35,13 +36,29 @@ def posts_detail(request,id=None):
     return render(request, "post_detail.html", context)
 
 def posts_list(request):
-    queryset = Post.objects.all()
+
+    queryset_list = Post.objects.all().order_by("-timestamp")
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+    page_request_var = 'page'
+
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+
     context = {
         "object_list":queryset,
         "title":"List",
+        "page_request_var": page_request_var
         }
     
     return render(request, "post_list.html",context)
+
 
 def posts_update(request, id=None):
     
